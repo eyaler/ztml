346be7af62ba849b178eef6498cb58b80168def2
eyaler <eyalgruss@gmail.com>
2022-10-17 21:40:11 +0300

    remove unnecessary f-string

--- misc/minibook.py
+++ misc/minibook.py
@@ -7,6 +7,6 @@ from ztml.ztml import ztml
 
 
 with urlopen('https://xem.github.io/miniBook/example') as f:
-    out, result = ztml.ztml(f.read(), f'index.html', mtf=80, raw=True, validate=True)
+    out, result = ztml.ztml(f.read(), 'index.html', mtf=80, raw=True, validate=True)
     print(f'{len(out):,} B')
     assert not result
